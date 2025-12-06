a0429e1b9994446222b62c41d0862b7cd36a77da
Chris-edappady <chris.edappady123@gmail.com>
2025-12-06 17:17:57 +0400

    Update 6: Added CORS headers

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const { MongoClient } = require('mongodb');
 
 // Initialize Express app
 const app = express();
+app.use(express.json());
+
+// Enable CORS for all requests
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Accept, X-Requested-With, Access-Control-Request-Method, Access-Control-Request-Headers'
+  );
+  next();
+});
 
 // Serve static images from the "images" folder
 app.use('/images', express.static(path.join(__dirname, 'images')));
@@ -12,19 +25,20 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 const uri = 'mongodb+srv://ce509:zn60iIQGQCibasri@cluster0.zhq0t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';  
 const client = new MongoClient(uri);
 
-// Function to connect to MongoDB
-async function connectDB() {
-  try {
-    await client.connect();
+let db;
+
+// Connect to MongoDB once at startup
+client
+  .connect()
+  .then(() => {
+    db = client.db('webstore');
     console.log('Connected to MongoDB');
-  } catch (err) {
-    console.error('MongoDB connection error:', err);
-  }
+  })
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 // Endpoint to get the lessons 
 app.get('/api/lessons', async (req, res) => {
-  try {
-    const db = client.db('webstore');  
+  try {  
     const lessons = await db.collection('lessons').find({}).toArray();
     res.json(lessons);
   } catch (err) {
@@ -32,4 +46,4 @@ app.get('/api/lessons', async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch lessons' });
   }
 });
-}
+
